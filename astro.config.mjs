003d14e61dd3c62d5f8690a53a104a7490b25743
Dave <dmsharpe5@gmail.com>
2026-07-15 14:51:55 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+	site: "https://blahg.netlify.app/",
+	base: "/",
+	integrations: [sitemap()],
+	markdown: {
+		shikiConfig: {
+			theme: "material-theme-darker",
+			langs: [],
+		},
+	},
+	compressHTML: true,
+});
